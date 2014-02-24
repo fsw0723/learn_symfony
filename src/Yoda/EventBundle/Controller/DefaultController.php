06ec41061f4a317c5b4d50982808eb292af959b7
Shuwei <sfang@thoughtworks.com>
2014-02-24 16:18:57 +0800

    add database with doctrine

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, $count)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
+        $event = $repo->findOneBy(array('name' => 'awesome event'));
+        return $this->render('EventBundle:Default:index.html.twig', array(
+            'name' => $name,
+            'count' => $count,
+            'event' => $event
+        ));
+//        $arr = array(
+//            'status' => 'good',
+//            'message' => 'Keep smiling'
+//        );
+//        $response = new Response(json_encode($arr));
+//        $response -> headers -> set('Content-Type', 'application/json');
+//        return $response;
+
     }
 }
