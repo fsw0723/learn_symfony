10694a3cbb22c815d2dbf366007a393a28247b0c
Shuwei <sfang@thoughtworks.com>
2014-02-25 17:51:29 +0800

    load user from database

---
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Yoda\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Yoda\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('user');
+        $user->setPassword($this->encodePassword($user, 'userpass'));
+
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setPassword($this->encodePassword($admin, 'adminpass'));
+        $admin->setRoles(array('ROLE_ADMIN'));
+
+        $manager->persist($user);
+        $manager->persist($admin);
+        $manager->flush();
+    }
+
+    private function encodePassword($user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     *
+     * @api
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+
+}
